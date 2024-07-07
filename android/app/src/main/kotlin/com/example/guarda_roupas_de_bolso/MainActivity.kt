c1d18ec71f654b6521d004512c9c6194f8d8a51e
AdrianoAnadinho <adrianoanadinho@gmail.com>
2024-07-07 11:02:17 -0300

    feat: project creation

---
+++ android/app/src/main/kotlin/com/example/guarda_roupas_de_bolso/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.guarda_roupas_de_bolso
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:guarda_roupas_de_bolso/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
